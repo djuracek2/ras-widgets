960b1f84398207b161dc5c0c2cdde9eb95688168
djuracek2 <djuracek@blm.gov>
2025-01-08 18:30:47 -0700

    remove commented code

--- widgets/ras-spatial-reporter/src/runtime/main.tsx
+++ widgets/ras-spatial-reporter/src/runtime/main.tsx
@@ -1,21 +1,15 @@
+/* eslint-disable prefer-const */
 import { React, type AllWidgetProps } from 'jimu-core'
 import { useState, useEffect, useRef } from 'react'
 import config from '../configs/config.json'
-import { Checkbox, Label, Switch, TextInput, Button, ButtonGroup, Select, Option, CollapsablePanel, Radio } from 'jimu-ui'
+import { Label, Button, ButtonGroup, Select, Option } from 'jimu-ui'
 import { DatePicker } from 'jimu-ui/basic/date-picker'
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer'
 import Graphic from '@arcgis/core/Graphic'
-
-import AcceptModal from './modals/acceptmodal'
-import RejectModal from './modals/rejectmodal'
 import { array } from 'prop-types'
 
 const Main = ({ success, failure, sjmv }) => {
-  const [approve, setApprove] = useState(false)
-  const [reject, setReject] = useState(false)
-  const [isAccept, setIsAccept] = useState(false)
-  const [isReject, setIsReject] = useState(false)
   const [stateSel, handleStateSel] = useState(false)
   const [districtOffice, handleDistrictOffice] = useState(false)
   const [fieldOffice, handleFieldOffice] = useState(false)
@@ -23,27 +17,6 @@ const Main = ({ success, failure, sjmv }) => {
   const [districtOptions, setDistrictOptions] = useState([])
   const [officeOptions, setOfficeOptions] = useState([])
   const graphicLayerRef = useRef<GraphicsLayer>(null)
-
-  const toggleAcceptModal = () => { setIsAccept(!isAccept) }
-  const toggleRejectModal = () => { setIsReject(!isReject) }
-  const handleApproveRejectReset = () => {
-    setApprove(false)
-    setReject(false)
-  }
-
-  const acceptWorkflow = () => {
-    setIsAccept(false)
-  }
-
-  const rejectWorkflow = () => {
-    console.log('reject feature')
-
-    setIsReject(false)
-  }
-
-  const runQuery = () => {
-
-  }
 
   useEffect(() => {
     if (sjmv && !graphicLayerRef.current) {
@@ -53,37 +26,16 @@ const Main = ({ success, failure, sjmv }) => {
     }
   }, [sjmv])
 
-  const handleReset = () => {
-    // setFormData({
-    //   IdText: "",
-    //   officeText: "",
-    //   approvalMode: "",
-
-    // })
-    // setApprovedText('')
-    // setSuccess(false)
-    // setFailure(false)
-    // graphicLayerRef.current.removeAll()
+  function runQuery () {
+    console.log('query ran...')
+  }
+  function handleRefresh () {
+    console.log('refresh...')
   }
 
-  //   function zoomToState(stateVal) {
-  //     if(stateVal != "Select State")
-  //     {
-  //         var zoomStateTask = new QueryTask(widgetContext.appConfig.queryLayers.stateLayer)
-  //         var zoomQuery = new Query;
-  //         zoomQuery.outFields = ["*"];
-  //         zoomQuery.returnGeometry = true;
-  //         var queryString = "ADMIN_ST = '" + stateVal + "'";
-  //         zoomQuery.where  =   queryString;
-  //         zoomStateTask.execute(zoomQuery, this.zoomSectionResults);
-  //     }
-  //     else{
-  //         widgetContext.shelter.hide();
-  //         widgetContext.map.graphics.clear();
-  //         widgetContext.map.setExtent(widgetContext.map.extent);
-  //     }
-  // }
-
+  function handleCancel () {
+    console.log('handle reset...')
+  }
   function zoomToDistrict () {
     const DistrictLayerUrl = config.queryLayers.districtLayer
     const DistrictLayer = new FeatureLayer({ url: DistrictLayerUrl })
@@ -195,14 +147,6 @@ const Main = ({ success, failure, sjmv }) => {
         const features = result.features
         console.log(features)
         setOfficeOptions(features)
-        // if (sjmv) {
-        //   sjmv.view.goTo({
-        //     target: result.features[0],
-        //     zoom: 16
-        //   }).catch(function (error) {
-        //     console.log('Error querying feature service.')
-        //   })
-        // }
       }
     })
   }
@@ -243,7 +187,28 @@ const Main = ({ success, failure, sjmv }) => {
       if (result.features.length > 0) {
         console.log(result.features)
         const features = result.features[0]
-        console.log(features)
+        const symbol = {
+          type: 'simple-fill', // autocasts as new SimpleFillSymbol()
+          color: [0, 204, 255, 0.4],
+          style: 'solid',
+          outline: {
+            color: [0, 204, 255, 0.8],
+            width: 2
+          }
+        }
+
+        const graphic = new Graphic({
+          geometry: features.geometry,
+          attributes: features.attributes,
+          symbol: symbol
+        })
+
+        if (graphicLayerRef.current) {
+          graphicLayerRef.current.removeAll()
+          graphicLayerRef.current.add(graphic)
+        } else {
+          console.error('Graphics layer not initialized.')
+        }
         // features.attributes.EMAIL_ID = 'Derek Test'
         // if (sjmv) {
         sjmv.view.goTo({
@@ -257,55 +222,6 @@ const Main = ({ success, failure, sjmv }) => {
     queryDistrict()
     queryOffice()
   }, [stateSel])
-
-  //zooms to plss search results
-  //  zoomSectionResults: function (results) {
-
-  //     if (results.features.length > 0) {
-  //         widgetContext.map.graphics.clear();
-
-  //         var symbol =  new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_NULL,
-  //                 new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new esri.Color([55, 0, 200]), 2),
-  //                 new esri.Color([255, 255, 0, 0.25]));
-
-  //         //Geometry Display
-
-  //         var geometry = results.features[0].geometry;
-  //         var graphic = new Graphic(geometry, symbol);
-  //         widgetContext.map.graphics.add(graphic);
-  //         widgetContext.map.setExtent(graphic._extent.expand(2));
-  //         widgetContext.shelter.hide();
-  //     } else {
-  //         window.alert("Sorry, but the application did not find valid State/district/Field Offices. Please try again.\n");
-  //     }
-  // },
-
-  // function zoomToDistrict(districtVal) {
-  //     if(districtVal != "Select District Office" &&  districtVal != "0")
-  //     {
-  //         var zoomdistrictTask = new QueryTask(widgetContext.appConfig.queryLayers.districtLayer)
-  //         var zoomQuery = new Query;
-  //         zoomQuery.outFields = ["*"];
-  //         zoomQuery.returnGeometry = true;
-  //         var queryString = "PARENT_CD = '" + districtVal + "'";
-  //         zoomQuery.where  =   queryString;
-  //         zoomdistrictTask.execute(zoomQuery, this.zoomSectionResults);
-  //     }
-  // }
-  // function zoomToOffice (officeVal) {
-  //     if(officeVal != "Select Field Office" && officeVal != "0")
-  //     {
-  //         var zoomOfficeTask = new QueryTask(widgetContext.appConfig.queryLayers.officeLayer)
-  //         var zoomQuery = new Query;
-  //         zoomQuery.outFields = ["*"];
-  //         zoomQuery.returnGeometry = true;
-  //         var queryString = "ADM_UNIT_CD = '" + officeVal + "'";
-  //         zoomQuery.where  =   queryString;
-  //         zoomOfficeTask.execute(zoomQuery, this.zoomSectionResults);
-  //         this._populateAllotmentGroup(officeVal);
-  //         this._populateAllotments(officeVal);
-  //     }
-  // }
 
   const handleStateChange = (event) => {
     handleStateSel(event.target.value)
@@ -417,8 +333,6 @@ const Main = ({ success, failure, sjmv }) => {
         </div>
       </div>
     <div>
-      {/* { success ? <Label className="success">Feature successfully updated!</Label> : ''}
-      { failure ? <Label className="unsuccessful">Feature was unable to updated.</Label> : ''} */}
     </div>
     <div className='d-flex justify-content-center'>
     <ButtonGroup size="default">
@@ -428,18 +342,16 @@ const Main = ({ success, failure, sjmv }) => {
         disabled={!inputValidation}
         onClick={runQuery}
       >
-        Approve
+        Search
       </Button>
-      <Button type="primary" disabled={!inputValidation} onClick={toggleRejectModal}>
-        Reject
+      <Button type="primary" disabled={!inputValidation} onClick={handleRefresh}>
+        Refresh
       </Button>
-      <Button type="primary" disabled={!inputValidation} onClick={handleReset}>
-        Reset
+      <Button type="primary" disabled={!inputValidation} onClick={handleCancel}>
+       Cancel
       </Button>
     </ButtonGroup>
     </div>
-    <AcceptModal isOpen={isAccept} toggle={toggleAcceptModal} acceptWorkflow={acceptWorkflow} />
-    <RejectModal isOpen={isReject} toggle= {toggleRejectModal} rejectWorkflow={rejectWorkflow} />
   </div>)
 }
 export default Main
